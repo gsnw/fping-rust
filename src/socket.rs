ad17e5ecdb64b986015fedfc72d61a1cde9f5015
German Service Network <support@gsnw.de>
2026-03-27 17:04:19 +0100

    fix: icmp checksum broken on little-endian systems

--- src/socket.rs
+++ src/socket.rs
@@ -117,11 +117,11 @@ fn icmp_checksum(data: &[u8]) -> u16 {
   let mut sum: u32 = 0;
   let mut i = 0;
   while i + 1 < data.len() {
-    sum += u16::from_be_bytes([data[i], data[i + 1]]) as u32;
+    sum += u16::from_ne_bytes([data[i], data[i + 1]]) as u32;
     i += 2;
   }
   if i < data.len() {
-    sum += (data[i] as u32) << 8;
+    sum += data[i] as u32;
   }
   while sum >> 16 != 0 {
     sum = (sum & 0xFFFF) + (sum >> 16);
